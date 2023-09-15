d178d7e78bb5a4efef839c6cead308ed33f0feec
Raushan Kumar <raushan.development@gmail.com>
2023-09-15 09:43:53 +0530

    added Prepared Statement

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String sql = "SELECT * FROM ACTORS;";
+        String sql = "SELECT * FROM Actors;";
         String url = "jdbc:postgresql://localhost:49153/postgres";
         String username = "postgres";
         String password = "postgrespw";
+
+        String insertSql = "INSERT INTO Actors values (?, ?, ?, ?, ?)";
 
         try {
             // Load the PostgreSQL JDBC driver
@@ -18,7 +20,6 @@ public class Main {
             Statement st = con.createStatement();
 
             /* Returns: a ResultSet object that contains the data produced by the given query; never null*/
-            ResultSet rs = st.executeQuery(sql); // Read Operations
             // For operations like Insert, Update and Delete, we use executeUpdate()
 
             // If a 'SELECT' query is executed, then we can process the results from the 'ResultSet'.
@@ -34,6 +35,16 @@ public class Main {
             //
 
             // Iterating through the result set and extracting the data;
+            PreparedStatement pst = con.prepareStatement(insertSql);
+            pst.setInt(1, 7);
+            pst.setString(2, "Jhon");
+            pst.setString(3, "Dairy");
+            pst.setInt(4, 35);
+            pst.setString(5, "Male");
+
+            int count = pst.executeUpdate(); // returns the number of rows affected;
+            ResultSet rs = st.executeQuery(sql); // Read Operations
+
 
             while(rs.next()){
                 int id = rs.getInt("id");
